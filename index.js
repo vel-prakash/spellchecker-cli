827f1f846db4115d2c7b90227517d30cd90d7e75
Vel Prakash <velprakashr08@gmail.com>
2026-05-04 21:52:42 +0530

    feat: added ability to pipe data to spellchk.

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ const { Command } = require("commander");
 
 const program = new Command();
 
-program.name("spellchk").option("-d, --dictionary").argument("<string>");
+program
+  .name("spellchk")
+  .option("-d, --dictionary")
+  .argument("[string]")
+  .action(async (text) => {
+    if (!text) {
+      let pipedData = '';
+      process.stdin.setEncoding('utf8');
+      
+      for await (const chunk of process.stdin) {
+        pipedData += chunk;
+      }
+
+      console.log(pipedData.trim())
+    } else {
+      console.log(text);
+    }
+  });
 
 program.parse();
 
-console.log(program.args[0]);
+// console.log(program.args[0]);
